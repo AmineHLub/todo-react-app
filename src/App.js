388b2a4b373a9c0d88d98b4901edc34779ca9c39
AmineHLub <Amino.bhl@gmail.com>
2021-10-27 11:16:53 +0100

    Add routing,Add about component, Add navbutton to about page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Main from './components/main';
 import Navbar from './components/navbar';
+import About from './about';
 
 function App() {
   const [open, OpenNav] = useState(false);
   return (
-    <div>
-      {open && <Navbar OpenNav={OpenNav} open={open} />}
-      <Main OpenNav={OpenNav} open={open} />
-    </div>
+    <Router>
+      <div>
+        {open && <Navbar OpenNav={OpenNav} open={open} />}
+        <Switch>
+          <Route exact path="/">
+            <Main OpenNav={OpenNav} open={open} />
+          </Route>
+          <Route path="/about">
+            <About OpenNav={OpenNav} open={open} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../style/navbar.css';
+import { Link } from 'react-router-dom';
 
 // eslint-disable-next-line react/prop-types
 const Navbar = ({ OpenNav, open }) => {
@@ -13,8 +14,8 @@ const Navbar = ({ OpenNav, open }) => {
       <nav className="navbar-container">
         <button className="close-hamburger" type="button" onClick={closeNav}> X </button>
         <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/">About</a></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
         </ul>
       </nav>
     </>
